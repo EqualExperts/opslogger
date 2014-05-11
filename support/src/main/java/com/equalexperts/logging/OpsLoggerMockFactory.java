aaba429eaa81fcf19f4700e0728b65ef32dc0981
seanjreilly <seanjreilly@gmail.com>
2014-05-11 22:20:16 +0100

    OpsLoggerMockFactory uses OpsLoggerTestDouble
    OpsLoggerMockFactory leverages the mock-agnostic
    OpsLoggerTestDouble, and adds a Mockito spy for easy
    verification of calls.

--- support/src/main/java/com/equalexperts/logging/OpsLoggerMockFactory.java
+++ support/src/main/java/com/equalexperts/logging/OpsLoggerMockFactory.java
@@ -1,13 +1,6 @@
 package com.equalexperts.logging;
 
-import org.hamcrest.CoreMatchers;
-import org.mockito.Mockito;
-
-import java.util.Arrays;
-import java.util.MissingFormatArgumentException;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.spy;
 
 /**
  * A factory that creates Mockito mocks of an OpsLogger instance and configures them to perform
@@ -15,62 +8,7 @@ import static org.junit.Assert.assertThat;
  */
 public class OpsLoggerMockFactory {
 
-    public static <T extends Enum<T> & LogMessage> OpsLogger<T> mockLogger(Class<T> logMessagesClass) {
-        @SuppressWarnings("unchecked")
-        OpsLogger<T> result = (OpsLogger<T>) Mockito.mock(OpsLogger.class);
-
-        Mockito.doAnswer(invocation -> {
-            Object[] arguments = invocation.getArguments();
-            T logMessageInstance = validateLogMessage(arguments);
-
-            Object[] formatStringArguments = Arrays.copyOfRange(arguments, 1, arguments.length);
-
-            checkForTooManyFormatStringArguments(logMessageInstance, formatStringArguments);
-
-            return String.format(logMessageInstance.getMessagePattern(), formatStringArguments);
-        }).when(result).log(Mockito.any(logMessagesClass), Mockito.anyVararg());
-
-        Mockito.doAnswer(invocation -> {
-            Object[] arguments = invocation.getArguments();
-            T logMessageInstance = validateLogMessage(arguments);
-
-            Throwable throwableInstance = (Throwable) arguments[1];
-            assertNotNull("Throwable instance must be provided", throwableInstance);
-
-            Object[] formatStringArguments = Arrays.copyOfRange(arguments, 2, arguments.length);
-
-            checkForTooManyFormatStringArguments(logMessageInstance, formatStringArguments);
-
-            return String.format(logMessageInstance.getMessagePattern(), formatStringArguments);
-        }).when(result).log(Mockito.any(logMessagesClass), Mockito.any(Throwable.class), Mockito.anyVararg());
-
-        return result;
-    }
-
-    private static <T extends Enum<T> & LogMessage> void checkForTooManyFormatStringArguments(T logMessageInstance, Object[] formatStringArguments) {
-        if (formatStringArguments.length > 0) {
-            /*
-                Check for too many arguments by removing one, and expecting "not enough arguments" to happen.
-             */
-            try {
-                //noinspection ResultOfMethodCallIgnored
-                String.format(logMessageInstance.getMessagePattern(), Arrays.copyOfRange(formatStringArguments, 0, formatStringArguments.length - 1));
-                throw new IllegalArgumentException("Too many format string arguments provided");
-            } catch (MissingFormatArgumentException expected) {
-                //expected
-            }
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T extends Enum<T> & LogMessage> T validateLogMessage(Object[] invocationArguments) {
-        T logMessageInstance = (T) invocationArguments[0];
-        assertNotNull("LogMessage must be provided", logMessageInstance);
-        assertNotNull("MessageCode must be provided", logMessageInstance.getMessageCode());
-        assertThat("MessageCode must be provided", logMessageInstance.getMessageCode(), CoreMatchers.not(""));
-        assertNotNull("MessagePattern must be provided", logMessageInstance.getMessagePattern());
-        assertThat("MessagePattern must be provided", logMessageInstance.getMessagePattern(), CoreMatchers.not(""));
-
-        return logMessageInstance;
+    public static <T extends Enum<T> & LogMessage> OpsLogger<T> mockLogger(@SuppressWarnings("UnusedParameters") Class<T> logMessagesClass) {
+        return spy(new OpsLoggerTestDouble<>());
     }
 }
--- support/src/test/java/com/equalexperts/logging/OpsLoggerMockFactoryTest.java
+++ support/src/test/java/com/equalexperts/logging/OpsLoggerMockFactoryTest.java
@@ -2,12 +2,9 @@ package com.equalexperts.logging;
 
 import org.junit.Test;
 
-import java.util.IllegalFormatException;
+import java.util.MissingFormatArgumentException;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class OpsLoggerMockFactoryTest {
@@ -15,190 +12,28 @@ public class OpsLoggerMockFactoryTest {
     private final OpsLogger<TestMessages> logger = OpsLoggerMockFactory.mockLogger(TestMessages.class);
 
     @Test
-    public void mockLogger_shouldWorkWithMockitoVerification_givenACallToLog() throws Exception {
-
+    public void mockLogger_shouldCreateAMockitoSpy() throws Exception {
         logger.log(TestMessages.Foo);
-
         verify(logger).log(TestMessages.Foo);
+        assertTrue(mockingDetails(logger).isSpy());
     }
 
     @Test
-    public void mockLogger_shouldThrowAnExceptionWhenNotEnoughFormatStringArgumentsAreProvided_givenACallToLog() throws Exception {
+    public void mockLogger_shouldReturnATestDoubleToEnforceAllOtherValidation() throws Exception {
+        assertTrue(logger instanceof OpsLoggerTestDouble);
+
         try {
+            //double-check validation is occurring by trying to log a message without arguments
             logger.log(TestMessages.Bar);
-            fail("expected an exception");
-        } catch (IllegalFormatException e) {
-            //this exception is expected
+            fail("Expected a failure");
+        } catch (MissingFormatArgumentException ignore) {
         }
     }
 
-    @Test
-    public void mockLogger_shouldThrowAnExceptionWhenTooManyFormatStringArgumentsAreProvided_givenACallToLog() throws Exception {
-        try {
-            logger.log(TestMessages.Bar, "Foo", "Bar");
-            fail("expected an exception");
-        } catch (IllegalArgumentException e) {
-            //this exception is expected
-            assertEquals("Too many format string arguments provided", e.getMessage());
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldCorrectlyAllowLogMessagesWithTwoOrMoreFormatStringArguments_givenACallToLog() throws Exception {
-        logger.log(TestMessages.MessageWithMultipleArguments, "Foo", "Bar");
-
-        verify(logger).log(TestMessages.MessageWithMultipleArguments, "Foo", "Bar");
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullLogMessage_givenACallToLog() throws Exception {
-        try {
-            logger.log(null);
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("LogMessage must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullMessageCode_givenACallToLog() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidNullCode);
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessageCode must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonEmptyMessageCode_givenACallToLog() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidEmptyCode);
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessageCode must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullMessageFormat_givenACallToLog() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidNullFormat);
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessagePattern must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonEmptyMessageFormat_givenACallToLog() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidEmptyFormat);
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessagePattern must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldThrowAnExceptionWhenNotEnoughFormatStringArgumentsAreProvided_givenACallToLogThatIncludesAThrowable() throws Exception {
-
-        try {
-            logger.log(TestMessages.Bar, new RuntimeException());
-            fail("expected an exception");
-        } catch (IllegalFormatException e) {
-            //this exception is expected
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldThrowAnExceptionWhenTooManyFormatStringArgumentsAreProvided_givenACallToLogThatIncludesAThrowable() throws Exception {
-
-        try {
-            logger.log(TestMessages.Bar, new RuntimeException(), "Foo", "Bar");
-            fail("expected an exception");
-        } catch (IllegalArgumentException e) {
-            //this exception is expected
-            assertEquals("Too many format string arguments provided", e.getMessage());
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldCorrectlyAllowLogMessagesWithTwoOrMoreFormatStringArguments_givenACallToLogThatIncludesAThrowable() throws Exception {
-        RuntimeException expectedException = new RuntimeException();
-        logger.log(TestMessages.MessageWithMultipleArguments, expectedException, "Foo", "Bar");
-
-        verify(logger).log(TestMessages.MessageWithMultipleArguments, expectedException, "Foo", "Bar");
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullLogMessage_givenACallToLogThatIncludesAThrowable() throws Exception {
-        try {
-            logger.log(null, new RuntimeException());
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("LogMessage must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullThrowable_givenACallToLogThatIncludesAThrowable() throws Exception {
-        try {
-            logger.log(TestMessages.Bar, null, "a");
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("Throwable instance must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullMessageCode_givenACallToLogThatIncludesAThrowable() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidNullCode, new RuntimeException());
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessageCode must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonEmptyMessageCode_givenACallToLogThatIncludesAThrowable() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidEmptyCode, new RuntimeException());
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessageCode must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonNullMessageFormat_givenACallToLogThatIncludesAThrowable() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidNullFormat, new RuntimeException());
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessagePattern must be provided"));
-        }
-    }
-
-    @Test
-    public void mockLogger_shouldEnforceANonEmptyMessageFormat_givenACallToLogThatIncludesAThrowable() throws Exception {
-        try {
-            logger.log(TestMessages.InvalidEmptyFormat, new RuntimeException());
-            fail("expected an exception");
-        } catch (AssertionError e) {
-            assertThat(e.getMessage(), containsString("MessagePattern must be provided"));
-        }
-    }
 
     static enum TestMessages implements LogMessage {
         Foo("CODE-Foo", "No Fields"),
-        Bar("CODE-Bar", "One Field: %s"),
-        InvalidNullCode(null, "Blah"),
-        InvalidEmptyCode("", "Blah"),
-        InvalidNullFormat("CODE-InvalidNullFormat", null),
-        InvalidEmptyFormat("CODE-InvalidEmptyFormat", ""),
-        MessageWithMultipleArguments("CODE-MultipleArguments", "Multiple Format String Arguments: %s %s");
+        Bar("CODE-Bar", "One Field: %s");
 
         //region LogMessage implementation guts
         private final String messageCode;
