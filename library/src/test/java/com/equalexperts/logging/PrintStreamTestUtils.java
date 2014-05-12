b3f11fdd615ed209516e7fab520a1a8f3eb544e1
seanjreilly <seanjreilly@gmail.com>
2014-05-12 16:01:33 +0100

    Code cleanup

--- library/src/test/java/com/equalexperts/logging/PrintStreamTestUtils.java
+++ library/src/test/java/com/equalexperts/logging/PrintStreamTestUtils.java
@@ -11,12 +11,7 @@ public class PrintStreamTestUtils {
      * Uses reflection to return the OutputStream wrapped by a PrintStream
      */
     public static OutputStream getBackingOutputStream(PrintStream stream) throws Exception {
-        Class<FilterOutputStream> cls = FilterOutputStream.class;
-        Field field = Stream.of(cls.getDeclaredFields())
-                .filter(f -> f.getName().equals("out"))
-                .findFirst().get();
-        field.setAccessible(true);
-
+        Field field = getInternalField(FilterOutputStream.class, "out");
         return (OutputStream) field.get(stream);
     }
 
@@ -24,12 +19,18 @@ public class PrintStreamTestUtils {
      * Uses reflection to determine the autoFlush setting of a PrintStream
      */
     public static boolean getAutoFlush(PrintStream stream) throws Exception {
-        Class<PrintStream> cls = PrintStream.class;
+        Field field = getInternalField(PrintStream.class, "autoFlush");
+        return (boolean) field.get(stream);
+    }
+
+    /**
+     * Obtains a reference to a non-public field and makes it accessible
+     */
+    private static Field getInternalField(Class<?> cls, String fieldName) {
         Field field = Stream.of(cls.getDeclaredFields())
-                .filter(f -> f.getName().equals("autoFlush"))
+                .filter(f -> f.getName().equals(fieldName))
                 .findFirst().get();
         field.setAccessible(true);
-
-        return (boolean) field.get(stream);
+        return field;
     }
 }
