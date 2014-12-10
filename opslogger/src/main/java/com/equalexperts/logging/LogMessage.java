bd5a7ea594885b096f3d5464cdee356cd3afd219
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2014-12-10 14:07:36 +0100

    Add @params to satisfy Java 8 javadoc.

--- opslogger/src/main/java/com/equalexperts/logging/LogMessage.java
+++ opslogger/src/main/java/com/equalexperts/logging/LogMessage.java
@@ -2,13 +2,13 @@ package com.equalexperts.logging;
 
 public interface LogMessage {
     /**
-     * unique code for this particular log message.  Example "QA001"
+     * @return unique code for this particular log message.  Example "QA001"
      */
 
     String getMessageCode();
 
     /**
-     * message pattern for this log message. Example "%d out of %d processed."
+     * @return message pattern for this log message. Example "%d out of %d processed."
      */
 
     String getMessagePattern();
