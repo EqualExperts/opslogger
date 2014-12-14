e4760c555dfcdf5387c85c88182c2630d3944601
Sean Reilly <seanjreilly@gmail.com>
2014-12-14 16:47:12 +0000

    Improve javadoc

--- opslogger/src/main/java/com/equalexperts/logging/OpsLoggerFactory.java
+++ opslogger/src/main/java/com/equalexperts/logging/OpsLoggerFactory.java
@@ -53,7 +53,8 @@ public class OpsLoggerFactory {
         return this;
     }
 
-    /** <p>Should stack traces be placed in individual files or printed along with the log statements?</p>
+    /**
+     * <p>Should stack traces be placed in individual files or printed along with the log statements?</p>
      * <p>If called with true, each unique stack trace will placed in its own file. (see setStackTraceStoragePath).
      * If called with false, stack traces will be printed to main log file.</p>
      * @param store true for separate files, false for inlined in log file
@@ -67,7 +68,8 @@ public class OpsLoggerFactory {
         return this;
     }
 
-    /** <p>Path to directory to contain individual stack trace files.</p>
+    /**
+     * <p>Path to directory to contain individual stack trace files.</p>
      * <p>
      * Must be called with a valid path corresponding to a directory, where stack traces will be stored
      * (see setStoreStackTracesInFilesystem).  If the directory does not exist, it will be created.</p>
@@ -81,7 +83,8 @@ public class OpsLoggerFactory {
         return this;
     }
 
-    /**<p>Handler for when exceptions occur when logging.</p>
+    /**
+     * <p>Handler for when exceptions occur when logging.</p>
      * <p>
      * If any exception is thrown "inside" this logger, it will caught and be passed on to this error handler,
      * which then is responsible for any further error handling.  The log message causing the error, will not
@@ -94,9 +97,10 @@ public class OpsLoggerFactory {
         return this;
     }
 
-    /**<p>Set the supplier of the map to print for each log entry.</p>
+    /**
+     * <p>Set the supplier of the map to print for each log entry.</p>
      * <p>The correlation id map is printed out as part of the message logged:</p>
-     *<p>Example code: (where <code>setCorrelationIdSupplier(()-&gt;map)</code> has been invoked in the OpsLoggerFactory
+     * <p>Example code: (where <code>setCorrelationIdSupplier(()-&gt;map)</code> has been invoked in the OpsLoggerFactory
      * invocation, and Failure has the message code "FOO-012345")</p>
      * <pre>
      * map.put("A", "113");
@@ -115,12 +119,18 @@ public class OpsLoggerFactory {
     }
 
     /**
-     * Enable/disable asynchronous logging.  When disabled, the log(...) method call does not return until the message
-     * is fully converted to a string and written to the target file/output stream.
+     * Enable/disable asynchronous logging.
+     *
+     * When disabled, the log(...) method call does not return until the message has been written to the target
+     * file/output stream.
+     *
      * When enabled, the log(...) method call pushes the log message object to an internal queue, and returns
-     * immediately.  The queue is emptied in order by a background thread.  This can be very useful for keeping response
+     * immediately.  The queue is emptied in order by a background thread. This can be very useful for keeping response
      * times low, but risks losing the log message objects still in the queue if the Java Virtual Machine is for any
      * reason abruptly terminated.
+     *
+     * If this method is not called, asynchronous logging is disabled.
+     *
      * @param async true=async, false=sync
      * @return <code>this</code> for further configuration
      */
@@ -129,7 +139,8 @@ public class OpsLoggerFactory {
         return this;
     }
 
-    /** Build and return the OpsLogger corresponding to the configuration provided.
+    /**
+     * Build and return the OpsLogger corresponding to the configuration provided.
      *
      * @param <T> LogMessage enum of all possible logger objects.
      * @return ready to use OpsLogger
