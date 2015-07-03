f57a50f39d5e8ce825281eae702df2ff3ac30458
seanjreilly <seanjreilly@gmail.com>
2015-07-03 22:56:42 +0100

    Reorganise example classes

--- sample-usage/src/main/java/uk/gov/gds/performance/collector/Main.java
+++ sample-usage/src/main/java/uk/gov/gds/performance/collector/Main.java
@@ -1,11 +1,18 @@
 package uk.gov.gds.performance.collector;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.equalexperts.logging.OpsLogger;
+import com.equalexperts.logging.OpsLoggerFactory;
+
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String... args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
-        ClassThatLogs classThatLogs = context.getBean("classThatLogs", ClassThatLogs.class);
-        classThatLogs.foo();
+    public static void main(String... args) throws Exception {
+        OpsLogger<CollectorLogMessage> logger = new OpsLoggerFactory()
+                .setDestination(System.out)
+                .setStackTraceStoragePath(Paths.get("/tmp/stacktraces"))
+                .build();
+
+        ClassThatLogs cls = new ClassThatLogs(logger);
+        cls.foo();
     }
 }
